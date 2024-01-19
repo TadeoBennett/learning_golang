5fd79b707fcf3d41fd1a87c97a6aeff1d9adb075
Tadeo Bennett <2021154344@ub.edu.bz>
2024-01-19 16:32:47 -0600

    create basic file server to load a page

--- main.go
+++ main.go
@@ -6,20 +6,12 @@ import (
 	"net/http"
 )
 
-// handler function
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World. We are topot."))
-}
-
 func main() {
-	//create a new serve mux
-	mux := http.NewServeMux() //used to register all mapping between URLs and functions between mappings that will handle them
-
-	//whenever the client types the root page, call the home function
-	mux.HandleFunc("/", home)
+	//create a file server(it loads the links to files if no .html is present)
+	fileserver := http.FileServer(http.Dir(".")) //look in the current directory
 
 	//create a web server responsible for listenting for requests
 	log.Println("starting server on port :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", fileserver)
 	log.Fatal(err)
 }
