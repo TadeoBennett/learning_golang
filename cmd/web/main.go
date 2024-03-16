1b30f0177be4d533ec29300d948bf49479ba2309
Tadeo Bennett <tadeos.bennett@gmail.com>
2024-03-15 19:29:59 -0600

    load env variables and using flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,22 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
+	"os"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"tadeobennett.net/quotation/pkg/models/postgresql"
 )
 
-// provide the credentials for our database
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "quotebox"
-	password = "tadeo2002"
-	dbname   = "quotebox"
-)
-
-func setUpDB() (*sql.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+func setUpDB(dsn string) (*sql.DB, error) {
 	// Establish a connection to the database
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
@@ -36,8 +27,15 @@ func setUpDB() (*sql.DB, error) {
 	return db, nil //return the connection and nil(no errors)
 }
 
-// Dependencies (things/variables)
-// DEPENDENCY INJECTION
+func loadEnvVariables() {
+	envFilePath := "../../.env"
+
+	err := godotenv.Load(envFilePath)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 type application struct {
 	quotes *postgresql.QuoteModel //references the QuoteModel which has the db connection
 }
@@ -46,12 +44,25 @@ func main() {
 	//tell go to accepts the http adrress from the user
 	// all commandline flags results will be stored in a pointerflag
 
-	//create a commnadline flag
+	//create a commandline flag
+	//to edit the provided address using the commandline, use 'go run . -addr=":number"'
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	loadEnvVariables(); //loads the variables in the .env file
+
+	host := os.Getenv("DB_HOST")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	user := os.Getenv("DB_USER")
+
+	//now you can provide a new dsn flag for the connection
+	dsn := flag.String("dsn",
+		"postgres://"+dbname+":"+password+"@"+host+"/"+user+"?sslmode=disable", "PostgreSQL DSN (Data Source Name)")
 	flag.Parse()
 
+
 	//FIRST CONNECT TO THE DATABASE --------------------------------
-	var db, err = setUpDB()
+	var db, err = setUpDB(*dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,8 +81,7 @@ func main() {
 		Handler: app.routes(), //return the multiplexer
 	}
 
-
-	//if the flag is not provided it will use port :4000 by default as specified in the flag. 
+	//if the flag is not provided it will use port :4000 by default as specified in the flag.
 	// To use another port, do the command: go run . -addr=":5000"
 	//to let the user see how to use the flag, run the command:  go run . -addr
 	log.Printf("Starting server on port %s", *addr)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module tadeobennett.net/quotation
 
 go 1.21.6
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
